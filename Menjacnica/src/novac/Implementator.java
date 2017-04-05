db02b9e54441d58dca8e4e6692589f1a3f712e2d
StefanKrstovic <stefan.krstovic@outlook.com>
2017-04-05 22:42:57 +0200

    Napravljena klasa Implementator sa nefunkcionalnim metodama

---
+++ Menjacnica/src/novac/Implementator.java
@@ -0,0 +1,27 @@
+package novac;
+
+import java.util.GregorianCalendar;
+
+import menjacnicaInterfejs.MenjacnicaInterfejs;
+
+public class Implementator implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKursZaDan(GregorianCalendar datum, double kupovni, double srednji, double prodajni) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKursZaDan(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public KursIDatum vratiKursZaDan(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
