9fdadd18d2821eb5b4eae5e86966c0fbe25ee8ef
StefanKrstovic <stefan.krstovic@outlook.com>
2017-04-05 22:49:13 +0200

    Dodat atribut lista i implementirane do kraja metode interfejsa

--- Menjacnica/src/menjacnicaInterfejs/MenjacnicaInterfejs.java
+++ Menjacnica/src/menjacnicaInterfejs/MenjacnicaInterfejs.java
@@ -1,11 +1,12 @@
 package menjacnicaInterfejs;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import novac.KursIDatum;
 
 public interface MenjacnicaInterfejs {
-	public void dodajKursZaDan(GregorianCalendar datum, double kupovni, double srednji, double prodajni);
-	public void obrisiKursZaDan(GregorianCalendar datum);
-	public KursIDatum vratiKursZaDan(GregorianCalendar datum);
+	public void dodajKursZaDan(GregorianCalendar datum, double kupovni, double srednji, double prodajni, LinkedList<KursIDatum> lista);
+	public void obrisiKursZaDan(GregorianCalendar datum,LinkedList<KursIDatum> lista);
+	public KursIDatum vratiKursZaDan(GregorianCalendar datum,LinkedList<KursIDatum> lista);
 }
--- Menjacnica/src/novac/Implementator.java
+++ Menjacnica/src/novac/Implementator.java
@@ -1,26 +1,38 @@
 package novac;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnicaInterfejs.MenjacnicaInterfejs;
 
 public class Implementator implements MenjacnicaInterfejs {
 
 	@Override
-	public void dodajKursZaDan(GregorianCalendar datum, double kupovni, double srednji, double prodajni) {
-		// TODO Auto-generated method stub
-
+	public void dodajKursZaDan(GregorianCalendar datum, double kupovni, double srednji, double prodajni, LinkedList<KursIDatum> lista) {
+		KursIDatum k = new KursIDatum();
+		k.setDatum(datum);
+		k.setKupovni(kupovni);
+		k.setProdajni(prodajni);
+		k.setSrednji(srednji);
+		lista.add(k);
 	}
 
 	@Override
-	public void obrisiKursZaDan(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+	public void obrisiKursZaDan(GregorianCalendar datum, LinkedList<KursIDatum> lista) {
+		for (int i = 0; i < lista.size(); i++) {
+			if(lista.get(i).getDatum().equals(datum)){
+				lista.remove(i);
+			}
+		}
 	}
 
 	@Override
-	public KursIDatum vratiKursZaDan(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+	public KursIDatum vratiKursZaDan(GregorianCalendar datum, LinkedList<KursIDatum> lista) {
+		for (int i = 0; i < lista.size(); i++) {
+			if(lista.get(i).getDatum().equals(datum)){
+				return lista.get(i);
+			}
+		}
 		return null;
 	}
 
