0246763810aa7646dce14629faba9827d241ebec
StefanKrstovic <stefan.krstovic@outlook.com>
2017-04-05 22:32:45 +0200

    Dodate metode equals, toString i hashCode

--- Menjacnica/src/novac/KursIDatum.java
+++ Menjacnica/src/novac/KursIDatum.java
@@ -1,5 +1,62 @@
 package novac;
 
-public class KursIDatum {
+import java.util.GregorianCalendar;
 
+public class KursIDatum {
+	private double prodajni;
+	private double kupovni;
+	private double srednji;
+	private GregorianCalendar datum;
+
+	public double getProdajni() {
+		return prodajni;
+	}
+
+	public void setProdajni(double prodajni) {
+		//if(prodajni<=0)throw new RuntimeException();
+		this.prodajni = prodajni;
+	}
+
+	public double getKupovni() {
+		return kupovni;
+	}
+
+	public void setKupovni(double kupovni) {
+		//if(kupovni<=0)throw new RuntimeException();
+		this.kupovni = kupovni;
+	}
+
+	public double getSrednji() {
+		return srednji;
+	}
+
+	public void setSrednji(double srednji) {
+		//if(srednji<=0)throw new RuntimeException();
+		this.srednji = srednji;
+	}
+
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+
+	public void setDatum(GregorianCalendar datum) {
+		//if(datum==null)throw new RuntimeException();
+		this.datum = datum;
+	}
+	
+
+	public boolean equals(KursIDatum kid) {
+		if(this.getKupovni()==kid.getKupovni() && this.prodajni==kid.getProdajni() && this.getSrednji()==kid.getSrednji())return true;
+		return false;
+	}
+
+	public String toString() {
+		return "Kurs za datum " + this.getDatum().getTime() + " je " + this.getKupovni()+ "\\" + this.getSrednji() + this.getProdajni();
+	}
+	
+	
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
 }
--- Menjacnica/src/novac/Valuta.java
+++ Menjacnica/src/novac/Valuta.java
@@ -1,5 +1,51 @@
 package novac;
 
-public class Valuta {
+import java.util.LinkedList;
 
+public class Valuta {
+	private String naziv;
+	private String skraceniNaziv;
+	private LinkedList<KursIDatum> kurs = new LinkedList<KursIDatum>();
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		//if(naziv==null || naziv=="") throw new RuntimeException();
+		this.naziv = naziv;
+	}
+
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		//if(skraceniNaziv==null || skraceniNaziv=="") throw new RuntimeException();
+		this.skraceniNaziv = skraceniNaziv;
+	}
+
+	public LinkedList<KursIDatum> getKurs() {
+		return kurs;
+	}
+
+	public void setKurs(KursIDatum kurs) {
+		//if(kurs==null)throw new RuntimeException();
+		this.kurs.add(kurs);
+	}
+	
+	
+	public boolean equals(Valuta v) {
+		return this.getNaziv().equals(v.getNaziv());
+	}
+	
+	@Override
+	public String toString() {
+		return "Naziv " + this.getNaziv() + " odnosno skraceno " + this.getSkraceniNaziv() + " poslednji unet kurs " + this.getKurs().getLast().getKupovni() + "\\" + this.getKurs().getLast().getSrednji() + "\\" +this.getKurs().getLast().getProdajni();
+	}
+	@Override
+	public int hashCode() {
+		
+		return super.hashCode();
+	}
 }
